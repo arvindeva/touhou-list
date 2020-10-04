860b9fe56c12d95fa275daa596515584fd86b06b
arvindeva <arvindeva@gmail.com>
2020-10-04 16:56:31 +0700

    Add slug to game

--- touhou-list-sanity/schemas/game.js
+++ touhou-list-sanity/schemas/game.js
@@ -10,5 +10,17 @@ export default {
       name: 'title',
       type: 'string',
     },
+    {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      liveEdit: true,
+      options: {
+        source: 'title',
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+      },
+    },
   ],
 };
